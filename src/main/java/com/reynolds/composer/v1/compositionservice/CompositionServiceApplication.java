b9567093ae6d20ced1c3512b652a46a2f1891fed
Lawrence Reynolds <lawrence.m.reynolds@googlemail.com>
2024-05-07 14:58:24 +0100

    Added Configuration to prevent Mongo auto-configuration.

--- src/main/java/com/reynolds/composer/v1/compositionservice/CompositionServiceApplication.java
+++ src/main/java/com/reynolds/composer/v1/compositionservice/CompositionServiceApplication.java
@@ -2,10 +2,15 @@ package com.reynolds.composer.v1.compositionservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+        MongoAutoConfiguration.class,
+        MongoDataAutoConfiguration.class
+})
 @ComponentScan("com.reynolds.composer.v1")
 @EntityScan("com.reynolds.composer.v1")
 public class CompositionServiceApplication {
